cfd5b2bb1f37938419c451d3c0320add09dff796
Kevin Nguyen <kevin.nguyen@hva.nl>
2026-02-23 00:26:34 +0100

    Fixed Angry Archaeologist from not rendering

--- src/main/kotlin/gobby/features/dungeons/MiniBossEsp.kt
+++ src/main/kotlin/gobby/features/dungeons/MiniBossEsp.kt
@@ -6,6 +6,7 @@ import gobby.features.render.EntityHighlighter
 import gobby.utils.LocationUtils.inBoss
 import gobby.utils.LocationUtils.inDungeons
 import net.minecraft.entity.Entity
+import net.minecraft.entity.decoration.ArmorStandEntity
 import net.minecraft.entity.player.PlayerEntity
 import java.awt.Color
 
@@ -23,13 +24,25 @@ object MiniBossEsp : EntityHighlighter() {
 
     override fun shouldHighlight(entity: Entity): Boolean {
         if (!inDungeons || inBoss) return false
+
+        if (entity is ArmorStandEntity) {
+            val customName = entity.customName?.string ?: ""
+            if (customName.contains("Angry Archaeologist")) return true
+        }
+
         if (entity !is PlayerEntity) return false
         if (entity == mc.player) return false
-//        if (entity.uuid.version() != 2) return false
         if (entity.isDead || entity.isSleeping) return false
         val name = entity.name.string
         val customName = entity.customName?.string ?: ""
         return MINIBOSS_NAMES.any { name.contains(it) || customName.contains(it) }
+    }
+
+    override fun resolveEntity(entity: Entity): Entity? {
+        if (entity is ArmorStandEntity) {
+            return getCorrespondingMob(entity)
+        }
+        return entity
     }
 
     override fun getColor(): Color = GobbyConfig.miniBossEspColor
