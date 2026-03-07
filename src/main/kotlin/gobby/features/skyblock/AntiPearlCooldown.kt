b99f8eb6c9f19a3926622a847dba2427f2722535
Kevin Nguyen <kevin.nguyen@hva.nl>
2026-03-07 16:43:24 +0100

    Clean up

--- src/main/kotlin/gobby/features/skyblock/AntiPearlCooldown.kt
+++
@@ -1,6 +0,0 @@
-package gobby.features.skyblock
-
-import gobby.gui.click.Category
-import gobby.gui.click.Module
-
-object AntiPearlCooldown : Module("Anti Pearl Cooldown", "Prevents the pearl cooldown from being applied", Category.SKYBLOCK)
